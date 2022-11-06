09903b854740de4cd7f424db2ba0c51e3b38a3a4
Viddernas <84942319+LucaslPedrosa@users.noreply.github.com>
2022-11-06 16:57:36 -0300

    TODO: Everything

---
+++ Principal.java
@@ -0,0 +1,70 @@
+/**
+ *  @Authors 
+ *    : Lucas Pedrosa
+ *    : Tacio Patez
+ *    : Matheus Spinolla
+ *    : Gabriel de Souza
+ *    
+ *
+ * 
+ * 
+ *  Name: Principal.java 
+ *
+ *  Principal.java class is the main class from the physical layer
+ *  simulation, it should be used
+ *  to extend the Application method and use it to launch javaFX
+ *  application
+ * 
+ *
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  private FXMLLoader loader;
+  private Parent root;
+  private static Scene scene;
+
+  /*
+   * The principal class will handle the lauch of the javaFX
+   * application.
+   * For the interface it extends the Application abstract class
+   * 
+   * The Application will:
+   * Construct a instance of Application class
+   * Calls the Init and start methods
+   * Wait for the Application to finish
+   * 
+   */
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    try {
+      loader = new FXMLLoader(getClass().getResource("/view/main_view.fxml"));
+      root = loader.load();
+      scene = new Scene(root);
+
+
+      primaryStage.getIcons().add(new Image("img/icon.png"));
+      primaryStage.setResizable(false);
+      primaryStage.setTitle("Hospital ideal");
+      primaryStage.setScene(scene);
+      primaryStage.sizeToScene();
+      primaryStage.show();
+
+    } catch (Exception e) {
+      System.out.println("arranca por la derecha el genio del futbol mundial");
+      System.out.println(e.getMessage());
+    }
+  }
+
+}
---
+++ controller/MainController.java
@@ -0,0 +1,75 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class MainController implements Initializable {
+
+  @FXML
+  private Button loginButton;
+
+  //These are used for changing the scenes
+  private Stage stage;
+  private Scene scene;
+  private Parent root;
+
+  
+
+
+  /* (non-Javadoc)
+   * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
+   */
+  @Override
+    public void initialize(URL location, ResourceBundle resources) {
+      
+      /*
+       * LOGIN BUTTON METHODS ****************************************************
+       * 
+       */
+
+      //change the colour of the button when the mouse is over it
+      loginButton.setOnMouseEntered(Event -> {
+        loginButton.setStyle("-fx-background-color: #77ff00;-fx-background-radius: 100;");
+      });
+
+      //change the colour of the button when the mouse is over it
+      loginButton.setOnMouseExited(Event -> {
+        loginButton.setStyle("-fx-background-color: #00FF00;-fx-background-radius: 100;");
+      });
+
+      //change the scene when the button is clicked
+      loginButton.setOnAction(Event -> {
+        try {
+          switchToMenu(Event);
+        } catch (Exception e) {
+          System.out.println("Cant login in, try being a better programmer");
+        }
+      });
+
+    }
+
+
+    public void switchToMenu(ActionEvent event) {
+      try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/menu_view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        root = loader.load();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+      } catch (Exception e) {
+        // TODO: handle exception
+      }
+    }
+
+}
