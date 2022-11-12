f4107541531f19213f8da41bd904e7878878183e
Viddernas <84942319+LucaslPedrosa@users.noreply.github.com>
2022-11-12 17:12:44 -0300

    TODO: Everything

---
+++ model/User.java
@@ -0,0 +1,30 @@
+package model;
+
+public class User{
+
+  private String login;
+  private String password;
+  private String name;
+  private String department;
+  private String birthDate;
+  private String CPF;
+
+  public User(){
+    this.login = "";
+    this.password = "";
+    this.name = "";
+    this.department = "";
+    this.birthDate = "";
+    this.CPF = "";
+  }
+
+  public User(String login, String password, String name, String department, String birthDate, String CPF){
+    this.login = login;
+    this.password = password;
+    this.name = name;
+    this.department = department;
+    this.birthDate = birthDate;
+    this.CPF = CPF;
+  }
+  
+}
