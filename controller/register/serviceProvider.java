f6654f537c2c7c79f1579f201bd379202b3cd703
TacioPatez <taciopatez@hotmail.com>
2022-11-20 19:52:09 -0300

    desfeito

--- controller/register/serviceProvider.java
+++ controller/register/serviceProvider.java
@@ -9,7 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class ServiceProvider implements Initializable{
+public class serviceProvider implements Initializable{
 
     @FXML
     private TextField nameField;
