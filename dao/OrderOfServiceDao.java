f31a8629df3cb44ba73369b68ded8fd9b8f06ecd
TacioPatez <taciopatez@hotmail.com>
2022-11-19 23:21:38 -0300

    OrderOfServiceDao

---
+++ dao/OrderOfServiceDao.java
@@ -0,0 +1,74 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.OrderOfService;
+
+public class OrderOfServiceDao extends OrderOfService {
+
+  public void register(String Number, String patientRecord, String dateAndHour, String referringDoctor, String department, String typeOfservice, String responsibleUser, String insurance){
+        String sql = "INSERT INTO OS" +
+                    "VALUES (?,?,?,?,?,?,?,?)";
+
+        try{
+            Connection con = ConnectionFactory.getConnection();
+            PreparedStatement smt = con.prepareStatement(sql);
+
+                smt.setString(1, Number);
+                smt.setString(2, patientRecord);
+                smt.setString(3,  dateAndHour);
+                smt.setString(4, referringDoctor);
+                smt.setString(5, department);
+                smt.setString(6, typeOfservice);
+                smt.setString(7, responsibleUser);
+                smt.setString(8, insurance);
+            smt.executeUpdate(); // Executa Comando no SQL
+            smt.close(); // Finaliza o PreparedStatement
+            con.close(); // Finaliza a Conexão com o BD
+        } catch (SQLException e){
+            System.out.println("Error ao Registrar a Ordem de Servico!");
+        }
+    }
+
+    public List<model.OrderOfService> getAll(){
+        List<model.OrderOfService> orders = null;
+        String sql = "SELECT *" +
+                    "FROM OS";
+
+        try{
+            Connection con = new ConnectionFactory().getConnection();
+            PreparedStatement smt = con.prepareStatement(sql);
+            ResultSet rs;
+
+            rs = smt.executeQuery(); // Executa Comando no SQL e rs recebe os valores
+
+            if(rs!=null){
+                while(rs.next()){
+                    model.OrderOfService o = new OrderOfService();
+                    o.setNumber(rs.getString("OS_NUMERO"));
+                    o.setPatientRecord(rs.getString("OS_PAC_REG"));
+                    o.setDateAndHour(rs.getString("OS_DTHR"));
+                    o.setReferringDoctor(rs.getString("OS_REQUISITANTE"));
+                    o.setDepartment(rs.getString("OS_STR_COD"));
+                    o.setTypeOfservice(rs.getString("OS_TIPO_ATEND"));
+                    o.setResponsibleUser(rs.getString("OS_USR_RESP"));
+                    o.setInsurance(rs.getString("OS_CNV_COD"));
+                    orders.add(o);
+                }
+            }
+
+            rs.close(); // Finaliza os dados da Query
+            smt.close(); // Finaliza o PreparedStatement
+            con.close(); // Finaliza a Conexão com o BD
+        } catch (SQLException e){
+            System.out.println("Error ao Buscar Todos as Ordens de Servico!");
+        }
+
+        return orders;
+    }
+
+}
--- model/OrderOfService.java
+++ model/OrderOfService.java
@@ -21,5 +21,79 @@ public class OrderOfService {
     this.responsibleUser = responsibleUser;
     this.insurance = insurance;
   }
-  
+
+  public OrderOfService(){
+    this.Number = "";
+    this.patientRecord = "";
+    this.dateAndHour = "";
+    this.referringDoctor = "";
+    this.department = "";
+    this.typeOfservice = "";
+    this.responsibleUser = "";
+    this.insurance = "";
+  }
+
+  public String getNumber() {
+    return Number;
+  }
+
+  public void setNumber(String number) {
+    Number = number;
+  }
+
+  public String getPatientRecord() {
+    return patientRecord;
+  }
+
+  public void setPatientRecord(String patientRecord) {
+    this.patientRecord = patientRecord;
+  }
+
+  public String getDateAndHour() {
+    return dateAndHour;
+  }
+
+  public void setDateAndHour(String dateAndHour) {
+    this.dateAndHour = dateAndHour;
+  }
+
+  public String getReferringDoctor() {
+    return referringDoctor;
+  }
+
+  public void setReferringDoctor(String referringDoctor) {
+    this.referringDoctor = referringDoctor;
+  }
+
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+
+  public String getTypeOfservice() {
+    return typeOfservice;
+  }
+
+  public void setTypeOfservice(String typeOfservice) {
+    this.typeOfservice = typeOfservice;
+  }
+
+  public String getResponsibleUser() {
+    return responsibleUser;
+  }
+
+  public void setResponsibleUser(String responsibleUser) {
+    this.responsibleUser = responsibleUser;
+  }
+
+  public String getInsurance() {
+    return insurance;
+  }
+
+  public void setInsurance(String insurance) {
+    this.insurance = insurance;
+  }
 }
