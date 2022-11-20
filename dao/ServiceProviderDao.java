7ec126561ab00386c86b1791b55cc529ff1475e7
TacioPatez <taciopatez@hotmail.com>
2022-11-19 23:12:21 -0300

    Tacinho Fofinho

---
+++ dao/ServiceProviderDao.java
@@ -0,0 +1,70 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.ServiceProvider;
+
+public class ServiceProviderDao extends ServiceProvider {
+
+  public void register(String code, String name, String CPF, String council, String type, String UF){
+        String sql = "INSERT INTO PSV" +
+                    "VALUES (?,?,?,?,?,?)";
+
+        try{
+            Connection con = ConnectionFactory.getConnection();
+            PreparedStatement smt = con.prepareStatement(sql);
+
+                smt.setString(1, code);
+                smt.setString(2, name);
+                smt.setString(3,  CPF);
+                smt.setString(4, council);
+                smt.setString(5, type);
+                smt.setString(6, UF);
+            smt.executeUpdate(); // Executa Comando no SQL
+            smt.close(); // Finaliza o PreparedStatement
+            con.close(); // Finaliza a Conexão com o BD
+        } catch (SQLException e){
+            System.out.println("Error ao Registrar o Prestador de Servico!");
+        }
+    }
+
+    public List<model.ServiceProvider> getAll(){
+        List<model.ServiceProvider> services = null;
+        String sql = "SELECT *" +
+                    "FROM PSV";
+
+        try{
+            Connection con = new ConnectionFactory().getConnection();
+            PreparedStatement smt = con.prepareStatement(sql);
+            ResultSet rs;
+
+            rs = smt.executeQuery(); // Executa Comando no SQL e rs recebe os valores
+
+            if(rs!=null){
+                while(rs.next()){
+                    model.ServiceProvider s = new ServiceProvider();
+                    s.setCode(rs.getString("PSV_COD"));
+                    s.setName(rs.getString("PSV_NOME"));
+                    s.setCPF(rs.getString("PSV_CPF"));
+                    s.setCouncil(rs.getString("PSV_CONSELHO"));
+                    s.setType(rs.getString("PSV_TIPO"));
+                    s.setUF(rs.getString("PSV_UF"));
+                    services.add(s);
+                }
+            }
+
+            rs.close(); // Finaliza os dados da Query
+            smt.close(); // Finaliza o PreparedStatement
+            con.close(); // Finaliza a Conexão com o BD
+        } catch (SQLException e){
+            System.out.println("Error ao Buscar Todos os Prestadores de Servico!");
+        }
+
+        return services;
+    }
+
+}
--- model/ServiceProvider.java
+++ model/ServiceProvider.java
@@ -16,5 +16,62 @@ public class ServiceProvider {
     this.type = type;
     this.UF = UF;
   }
+
+  public ServiceProvider(){
+    this.code = "";
+    this.name = "";
+    this.CPF = "";
+    this.council = "";
+    this.type = "";
+    this.UF = "";
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCPF() {
+    return CPF;
+  }
+
+  public void setCPF(String cPF) {
+    CPF = cPF;
+  }
+
+  public String getCouncil() {
+    return council;
+  }
+
+  public void setCouncil(String council) {
+    this.council = council;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getUF() {
+    return UF;
+  }
+
+  public void setUF(String uF) {
+    UF = uF;
+  }
   
 }
