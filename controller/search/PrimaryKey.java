58830fdc2148b39eceabf347b8d24cd7a071c370
Viddernas <84942319+LucaslPedrosa@users.noreply.github.com>
2022-11-22 14:27:45 -0300

    TODO: Entendendo

--- controller/search/PrimaryKey.java
+++ controller/search/PrimaryKey.java
@@ -3,6 +3,8 @@ package controller.search;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -24,12 +26,31 @@ public class PrimaryKey implements Initializable{
     @FXML
     private ChoiceBox<String> chaveChoice;
 
+    private ObservableList<String> codeTypes = FXCollections.observableArrayList(
+        "Buscar Usuario", //0
+"Buscar Setor",//1
+        "Buscar Equipamento", //2
+        "Buscar Convenio", //3
+        "Buscar Paciente", //4
+        "Buscar Prestador", //5
+        "Buscar Ordem"); //6
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
-        
 
+        chaveChoice.setItems(codeTypes);
+        chaveChoice.setValue("Buscar Departamento");
         
+        registerButton.setOnAction(Event -> {
+            getCodeTypes();
+        });
+
+    }
+
+    public int getCodeTypes(){
+        System.out.println(chaveChoice.getSelectionModel().getSelectedIndex());
+        return chaveChoice.getSelectionModel().getSelectedIndex();
     }
 
 }
